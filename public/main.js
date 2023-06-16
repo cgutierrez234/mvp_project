806c8411c722816654ac01f635713978f8dd1dda
cgutierrez234 <113813911+cgutierrez234@users.noreply.github.com>
2023-06-16 14:14:08 -0600

    added buttons to goal cards

--- public/main.js
+++ public/main.js
@@ -5,26 +5,26 @@ const API_URL = "https://mvp-project-web-service.onrender.com";
 const getGoals = async () => {
   const response = await fetch(`${API_URL}/goals`);
   const data = await response.json();
-  goalCards(data);
+  createGoalCards(data);
 };
 
-const goalCards = (data) => {
+const createGoalCards = (data) => {
   const goalsContainer = document.getElementById("goalsContainer");
-
+  goalsContainer.innerHTML = "";
   for (let goal of data) {
     const goalCard = document.createElement("div");
     goalCard.setAttribute("id", `${goal.id}`);
     goalCard.textContent = `${goal.goal}`;
     goalsContainer.appendChild(goalCard);
+
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    goalCard.appendChild(editButton);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    goalCard.appendChild(deleteButton);
   }
 };
-
-// DOM variables
-const fetchGoals = document.getElementById("goalBtn");
-
-// Event Listeners
-// fetchGoals.addEventListener("click", () => {
-//   getGoal();
-// });
 
 getGoals();
