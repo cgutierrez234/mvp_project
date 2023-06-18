152b4c77aaba021639a12804a0a2fbbf2ec05e23
cgutierrez234 <113813911+cgutierrez234@users.noreply.github.com>
2023-06-17 20:41:08 -0600

    tweaking createGoal function

--- public/main.js
+++ public/main.js
@@ -21,7 +21,6 @@ const createGoal = async (goal) => {
   };
   const response = await fetch(`${API_URL}/goals`, options);
   const data = await response.json();
-  goalCard.innerHTML = "";
   createGoalCards(data);
 };
 
